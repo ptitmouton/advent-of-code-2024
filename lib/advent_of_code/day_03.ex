52be37b464c7fc2d9e334bde9568cd70eb82fbdd
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2024-12-03 07:31:45 +0100

    Day 3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,28 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(input) do
+    regex = ~r/mul\((\d{1,3}),(\d{1,3})\)/
+
+    regex
+    |> Regex.scan(input)
+    |> Enum.map(fn [_, a, b] ->
+      {String.to_integer(a), String.to_integer(b)}
+    end)
+    |> Enum.reduce(0, fn {l, r}, acc ->
+      acc + l * r
+    end)
   end
 
-  def part2(_args) do
+  def part2(input) do
+    regex = ~r/(do(?:n't)?\(\))|mul\((\d{1,3}),(\d{1,3})\)/
+
+    regex
+    |> Regex.scan(input)
+    |> Enum.reduce({true, 0}, fn
+      [_, "don't()" | _], {_, acc} -> {false, acc}
+      [_, "do()" | _], {_, acc} -> {true, acc}
+      [_, _, a, b], {true, acc} -> {true, acc + String.to_integer(a) * String.to_integer(b)}
+      [_, _, _, _], {false, acc} -> {false, acc}
+    end)
+    |> elem(1)
   end
 end
--- lib/mix/tasks/d03.p1.ex
+++ lib/mix/tasks/d03.p1.ex
@@ -1,11 +1,12 @@
 defmodule Mix.Tasks.D03.P1 do
+  @requirements ["app.start"]
   use Mix.Task
 
   import AdventOfCode.Day03
 
   @shortdoc "Day 03 Part 1"
   def run(args) do
-    input = nil
+    input = AdventOfCode.Input.get!(3)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
